19ae8502581365465a5e301e906d803c87c61200
Adam Elsodaney <adam.elso@gmail.com>
2014-11-10 19:48:43 +0000

    Allow driver to be null

--- src/Sylius/Bundle/ArchetypeBundle/DependencyInjection/Configuration.php
+++ src/Sylius/Bundle/ArchetypeBundle/DependencyInjection/Configuration.php
@@ -36,7 +36,7 @@ class Configuration implements ConfigurationInterface
         $rootNode
             ->addDefaultsIfNotSet()
             ->children()
-                ->scalarNode('driver')->isRequired()->cannotBeEmpty()->end()
+                ->scalarNode('driver')->defaultNull()->end()
             ->end()
         ;
 

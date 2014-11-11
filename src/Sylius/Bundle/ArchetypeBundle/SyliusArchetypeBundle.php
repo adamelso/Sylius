1b23137df6b58625b9d463a95061faaa86932e37
Adam Elsodaney <adam.elso@gmail.com>
2014-11-11 19:25:34 +0000

    Extend option classes for archetypes

--- src/Sylius/Bundle/ArchetypeBundle/SyliusArchetypeBundle.php
+++ src/Sylius/Bundle/ArchetypeBundle/SyliusArchetypeBundle.php
@@ -47,8 +47,8 @@ class SyliusArchetypeBundle extends AbstractResourceBundle
             'Sylius\Component\Archetype\Model\AttributeInterface'      => 'sylius.model.archetype_attribute.class',
             'Sylius\Component\Archetype\Model\AttributeValueInterface' => 'sylius.model.archetype_attribute_value.class',
 //            'Sylius\Component\Archetype\Model\VariantInterface'        => 'sylius.model.archetype_variant.class',
-//            'Sylius\Component\Archetype\Model\OptionInterface'         => 'sylius.model.archetype_option.class',
-//            'Sylius\Component\Archetype\Model\OptionValueInterface'    => 'sylius.model.archetype_option_value.class',
+            'Sylius\Component\Archetype\Model\OptionInterface'         => 'sylius.model.archetype_option.class',
+            'Sylius\Component\Archetype\Model\OptionValueInterface'    => 'sylius.model.archetype_option_value.class',
         );
     }
 
